93d398a856d15ede7b09534688a2974932796639
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-09-12 11:52:35 +0500

    all done

--- script.js
+++ script.js
@@ -4,41 +4,42 @@ for (const box of boxes) {
   // console.log(box);
   box.onmouseover = () => {
     box.style.backgroundColor = "coral";
+    box.innerHTML = "coral";
     box.style.transition = "0.2s";
 
     new Promise((resolve, reject) => {
       setTimeout(() => {
         box.style.backgroundColor = "red";
+        box.innerHTML = "red";
         resolve("green");
       }, 500);
     }).then((color) => {
       new Promise((resolve, reject) => {
         setTimeout(() => {
           box.style.backgroundColor = color;
+          box.innerHTML = color;
+
           // resolve("rgb(28, 24, 24)");
           resolve("blue");
         }, 500);
-      })
-        .then((color) => {
+      }).then((color) => {
+        return new Promise((resolve, reject) => {
           setTimeout(() => {
             box.style.backgroundColor = color;
             // box.style.transition = "2s";
+            box.innerHTML = color;
+
+            resolve("rgb(28, 24, 24)");
           }, 500);
-        })
-        .then((color) => {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              box.style.backgroundColor = color;
-              // box.style.transition = "2s";
-              resolve("rgb(28, 24, 24)");
-            }, 500);
-          }).then((color) => {
-            setTimeout(() => {
-              box.style.backgroundColor = color;
-              box.style.transition = "2s";
-            }, 500);
-          });
+        }).then((color) => {
+          setTimeout(() => {
+            box.style.backgroundColor = color;
+            box.innerHTML = "";
+            // box.innerHTML = "";
+            box.style.transition = "2s";
+          }, 500);
         });
+      });
     });
   };
 }
